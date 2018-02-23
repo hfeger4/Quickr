bbe64b103c886c9e940e17c7055b0e750e8db595
Howard Feger <hfeger4@gmail.com>
2018-02-22 18:24:58 -0800

    add folder for components, actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Gallery from './components/gallery.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello
+        <Gallery />
       </div>
     );
   }
---
+++ src/components/gallery.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Gallery extends Component{
+  render(){
+    return(<div>Hello There lol</div>);
+  }
+}
+
+export default Gallery;
